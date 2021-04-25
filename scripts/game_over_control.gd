53c75240164ec9e9c84d05e16b15dc35387c8452
Simon Berger <simon@siku2.io>
2021-04-25 16:51:26 +0200

    entering and leaving the shop

--- scripts/game_over_control.gd
+++ scripts/game_over_control.gd
@@ -1,9 +1,12 @@
 extends Control
 
+export var scene_playground: PackedScene
+export var scene_shop: PackedScene
 
-func _on_RestartButton_pressed():
-	get_tree().change_scene("res://scenes/playground.tscn")
+func _on_RestartButton_pressed() -> void:
+	var err := get_tree().change_scene_to(scene_playground)
+	assert(!err) 
 
-
-func _on_ShopButton_pressed():
-	print("shop")
+func _on_ShopButton_pressed() -> void:
+	var err := get_tree().change_scene_to(scene_shop)
+	assert(!err)
