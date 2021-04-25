de73cf1dd9258528811cf5a9cb2aeae6225dd77c
Simon Berger <simon@siku2.io>
2021-04-25 20:12:15 +0200

    harpoon launching

--- scripts/harpoon.gd
+++ scripts/harpoon.gd
@@ -1,13 +1,18 @@
 extends KinematicBody2D
 
-var _velocity := Vector2(500.0, -50.0)
-var _drag_coefficient := 10.0
-var _base_acceleration := Vector2(0.0, 200.0)
+export var active := false
+
+var _velocity := Vector2.ZERO
+var _drag_coefficient := 8.0
+var _base_acceleration := Vector2(0.0, 100.0)
 var _stuck_in: Node2D = null
 
 onready var _particles := $Particles2D
 
 func _physics_process(delta: float) -> void:
+	if !active:
+		return
+
 	if _stuck_in:
 		return
 
@@ -22,11 +27,19 @@ func _physics_process(delta: float) -> void:
 	else:
 		self.look_at(self.position + _velocity)
 
+func launch(strength: float) -> void:
+	active = true
+	_velocity = Vector2.RIGHT.rotated(self.rotation) * strength
+
 func _calculate_drag(velocity: Vector2) -> Vector2:
 	var inverse_direction := -velocity.normalized()
 	return inverse_direction * velocity.length_squared() * _drag_coefficient
 
 func _on_collision(collision: KinematicCollision2D) -> void:
+	var collider := collision.collider as Node2D
+	if collider.is_in_group("harpoon"):
+		return
+
 	_velocity = Vector2.ZERO
-	_stuck_in = collision.collider
+	_stuck_in = collider
 	_particles.emitting = false
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+const Harpoon := preload("./harpoon.gd")
+
+export var prefab_harpoon: PackedScene
 export (int) var speed = 15
 export (float) var friction = 0.05
 export (float) var acceleration = 0.02
@@ -17,8 +20,9 @@ onready var _player_upgrades := get_node(@"/root/PlayerUpgrades")
 var health : float = max_health
 var velocity := Vector2()
 var input_velocity := Vector2()
-
 var damage_in_progress = false
+var _aiming: bool
+var _harpoon: Harpoon = null
 
 func _ready():
 	health_bar.visible = false
@@ -28,19 +32,19 @@ func _ready():
 
 func get_input() -> void:
 	input_velocity = Vector2()
-	if Input.is_action_pressed('right'):
+	if Input.is_action_pressed("right"):
 		input_velocity.x += 2
-	if Input.is_action_pressed('left'):
+	if Input.is_action_pressed("left"):
 		input_velocity.x -= 2
-	if Input.is_action_pressed('up'):
+	if Input.is_action_pressed("up"):
 		input_velocity.y -= 1
-	if Input.is_action_pressed('down'):
+	if Input.is_action_pressed("down"):
 		input_velocity.y += 1.5
 	input_velocity = input_velocity * speed
 
 
 func calculate_velocity() -> Vector2:
-	if input_velocity.length() > 0:
+	if not _aiming and input_velocity.length() > 0:
 		return velocity.linear_interpolate(input_velocity, acceleration)
 	return velocity.linear_interpolate(Vector2(), friction)
 
@@ -72,6 +76,13 @@ func rotate_player() -> void:
 		sprite.set_flip_h(false)
 		self.rotation = velocity.angle()
 
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("aim"):
+		_aiming = true
+		_process_aiming()
+	elif _aiming:
+		_aiming = false
+		_launch_harpoon()
 
 func _physics_process(delta):
 	get_input()
@@ -86,10 +97,27 @@ func _physics_process(delta):
 	else:
 		$AnimatedSprite.animation = "swimming"
 
+func _process_aiming() -> void:
+	if not _harpoon:
+		_harpoon = prefab_harpoon.instance()
+		get_tree().current_scene.add_child(_harpoon)
+
+	_harpoon.global_position = self.global_position
+	_harpoon.look_at(_harpoon.global_position + _get_aiming_input())
+
+func _launch_harpoon() -> void:
+	assert(_harpoon)
+	_harpoon.launch(700.0)
+	_harpoon = null
 
 func _on_DamageTimer_timeout():
 	damage_in_progress = false
 
+func _get_aiming_input() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
 
 func _on_CoinDetectionArea_area_entered(area):
 	if area.get_parent().is_in_group("coin"):
