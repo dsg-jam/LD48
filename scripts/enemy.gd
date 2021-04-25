ed163ab371ba20f995348cbd3b051147643f1711
mineiwik <29404520+mineiwik@users.noreply.github.com>
2021-04-25 14:02:52 +0200

    Combat (#8)
    * Add enemy following player
    
    * Add bounce effect
    
    * Fixing naming part 1
    
    * Fixing naming part 2
    
    * Add player animations and health system (part 1)
    
    * Add coin detection

---
+++ scripts/enemy.gd
@@ -0,0 +1,92 @@
+extends KinematicBody2D
+
+# Enemy controller
+
+export (int) var idle_speed := 10
+export (int) var hunting_speed := 20
+
+export (float) var idle_acceleration := 0.01
+export (float) var hunting_acceleration := 0.04
+
+export (float) var friction = 0.01
+
+export (float) var health = 100.0
+export (float) var damage = 25.0
+
+export (float) var bounce_rate = 50.0
+
+onready var sprite := $AnimatedSprite
+
+var speed := idle_speed
+var acceleration := idle_acceleration
+
+var direction := Vector2(1, 0)
+var velocity := Vector2()
+
+var player : KinematicBody2D
+var is_hunting := false
+
+var rng = RandomNumberGenerator.new()
+
+
+func hunting_velocity() -> Vector2:
+	var x_direction = player.global_position.x - self.global_position.x
+	var y_direction = player.global_position.y - self.global_position.y
+	return Vector2(x_direction, y_direction).normalized() * speed
+
+
+func calculate_velocity() -> Vector2:
+	if direction.length() > 0:
+		return velocity.linear_interpolate(direction, acceleration)
+	return velocity.linear_interpolate(Vector2(), friction)
+
+
+func rotate_enemy() -> void:
+	if velocity.x < 0:
+		sprite.set_flip_h(true)
+		self.rotation = velocity.angle() + PI
+	else:
+		sprite.set_flip_h(false)
+		self.rotation = velocity.angle()
+
+
+func collision_control(collision) -> void:
+	if collision:
+		if collision.collider.name == "Player":
+			velocity = velocity.bounce(collision.normal).normalized() * bounce_rate
+			collision.collider.reduce_health(damage)
+		else:
+			direction.x *= -1
+			velocity.x *= -1
+			sprite.set_flip_h(!sprite.flip_h)
+
+
+func _physics_process(delta):
+	if is_hunting:
+		direction = hunting_velocity()
+	velocity = calculate_velocity()
+	rotate_enemy()
+	var collision = move_and_collide(velocity * speed * delta)
+	collision_control(collision)
+
+
+# Signals handler
+
+func _on_Timer_timeout():
+	rng.randomize()
+	direction.y = rng.randf_range(-0.5, 0.5)
+
+
+func _on_PlayerDetectionArea_body_entered(body):
+	if body.name == "Player":
+		player = body
+		is_hunting = true
+		speed = hunting_speed
+		acceleration = hunting_acceleration
+
+
+func _on_PlayerDetectionArea_body_exited(body):
+	if body.name == "Player":
+		is_hunting = false
+		speed = idle_speed
+		acceleration = idle_acceleration
---
+++ scripts/player.gd
@@ -0,0 +1,72 @@
+extends KinematicBody2D
+
+export (int) var speed = 15
+export (float) var friction = 0.05
+export (float) var acceleration = 0.02
+export (float) var health = 100.0
+
+onready var sprite := $AnimatedSprite
+
+var velocity := Vector2()
+var input_velocity := Vector2()
+
+var damage_in_progress = false
+
+func get_input() -> void:
+	input_velocity = Vector2()
+	if Input.is_action_pressed('right'):
+		input_velocity.x += 2
+	if Input.is_action_pressed('left'):
+		input_velocity.x -= 2
+	if Input.is_action_pressed('up'):
+		input_velocity.y -= 1
+	if Input.is_action_pressed('down'):
+		input_velocity.y += 1.5
+	input_velocity = input_velocity * speed
+
+
+func calculate_velocity() -> Vector2:
+	if input_velocity.length() > 0:
+		return velocity.linear_interpolate(input_velocity, acceleration)
+	return velocity.linear_interpolate(Vector2(), friction)
+
+
+func reduce_health(amount) -> void:
+	$AnimatedSprite.animation = "damage"
+	damage_in_progress = true
+	$DamageTimer.start()
+	health -= amount
+	print(health)
+
+
+func rotate_player() -> void:
+	if velocity.x < 0:
+		sprite.set_flip_h(true)
+		self.rotation = velocity.angle() + PI
+	else:
+		sprite.set_flip_h(false)
+		self.rotation = velocity.angle()
+
+
+
+func _physics_process(delta):
+	get_input()
+	velocity = calculate_velocity()
+	move_and_collide(velocity * speed * delta)
+	rotate_player()
+	if damage_in_progress:
+		return
+	if input_velocity == Vector2.ZERO and velocity.length() < 10:
+			velocity = Vector2()
+			$AnimatedSprite.animation = "idle"
+	else:
+		$AnimatedSprite.animation = "swimming"
+
+
+func _on_DamageTimer_timeout():
+	damage_in_progress = false
+
+
+func _on_CoinDetectionArea_area_entered(area):
+	if area.get_parent().get_node("Coin"):
+		area.get_parent().queue_free()
