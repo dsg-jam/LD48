bde5231e63c7e0eea47aef4b34231c90ba0768ea
Simon Berger <simon@siku2.io>
2021-04-26 21:05:31 +0200

    money tracking

--- scripts/playground_control.gd
+++ scripts/playground_control.gd
@@ -3,6 +3,9 @@ extends Node2D
 const LEFT_BARRIER = -400
 const RIGHT_BARRIER = 1300
 
+export var _label_height_path: NodePath
+export var _money_display_path: NodePath
+
 var rng = RandomNumberGenerator.new()
 var _player_depth: float = 0.0
 
@@ -11,7 +14,27 @@ var coin_prefab := preload("res://prefabs/coin.tscn")
 var enemy_prefab := preload("res://prefabs/enemies/bully.tscn")
 
 onready var player := $Player
+onready var _label_height := get_node(_label_height_path)
+onready var _money_display := get_node(_money_display_path)
 onready var _player_upgrades := get_node(@"/root/PlayerUpgrades")
+onready var _player_money_at_start: int = _player_upgrades.money
+
+func _ready() -> void:
+	var err = _player_upgrades.connect("money_changed", self, "_on_player_money_changed")
+	assert(!err)
+
+func _exit_tree() -> void:
+	var score = Score.new()
+	score.depth = _player_depth
+	score.money = _player_upgrades.money - _player_money_at_start
+	_player_upgrades.current_score = score
+
+func _process(_delta) -> void:
+	_player_depth = max(0, player.global_position.y / 100.0)
+	_label_height.text = "%.1fm" % _player_depth
+	
+	coin_spawn_manager(_player_depth)
+	enemy_spawn_manager(_player_depth)
 
 func enemy_hunting_speed(depth : float) -> float:
 	return exp(0.025 * (depth)) + 15
@@ -77,18 +100,8 @@ func coin_spawn_manager(depth : float) -> void:
 		rng.randomize()
 		coin_spawn(depth, Vector2(rng.randf_range(LEFT_BARRIER, RIGHT_BARRIER), rng.randf_range(player_depth + 500, player_depth + 2500)))
 
-func _exit_tree() -> void:
-	var score = Score.new()
-	score.depth = _player_depth
-	score.money = 0
-	_player_upgrades.current_score = score
-
-func _process(_delta) -> void:
-	_player_depth = max(0, player.global_position.y / 100.0)
-	get_node("CanvasLayer/VBoxContainer/HeightLabel").text = "%.1fm" % _player_depth
-	
-	coin_spawn_manager(_player_depth)
-	enemy_spawn_manager(_player_depth)
+func _on_player_money_changed() -> void:
+	_money_display.set_money_value(_player_upgrades.money - _player_money_at_start)
 
 func _on_DestroyTimer_timeout() -> void:
 	destroy_nodes()
